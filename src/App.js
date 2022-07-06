eaee538d2a7813f398a238c90c360ca3fc669579
tubi55 <tubi55@nate.com>
2022-07-06 17:06:02 +0900

    메인, 서브 header디자인 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 //common
 import Header from './components/common/Header';
@@ -21,12 +21,19 @@ import './scss/style.scss';
 function App() {
 	return (
 		<>
-			<Header />
+			{/* Switch 같은 경로의  router연결시 구체적인 라우터 하나만 적용한다. */}
+			<Switch>
+				<Route exact path='/'>
+					{/*  메인에만 적용되는  header */}
+					<Header type={'main'} />
+					<Visual />
+					<Content />
+				</Route>
 
-			<Route exact path='/'>
-				<Visual />
-				<Content />
-			</Route>
+				<Route path='/'>
+					<Header type={'sub'} />
+				</Route>
+			</Switch>
 
 			<Route path='/department' component={Department} />
 			<Route path='/youtube' component={Youtube} />
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,10 @@
-import React from 'react';
+//import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
 	const active = { color: 'aqua' };
 	return (
-		<header>
+		<header className={props.type}>
 			<div className='inner'>
 				<h1>
 					<NavLink exact to='/' activeStyle={active}>
