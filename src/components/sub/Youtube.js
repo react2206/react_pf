f35223cf3cab74bfe2cb9176183d8a35fb81fc45
tubi55 <tubi55@nate.com>
2022-07-07 14:15:05 +0900

    유튜브 데이터 axios로 호출

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,6 +1,21 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Youtube() {
+	const [Vids, setVids] = useState([]);
+
+	useEffect(() => {
+		const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
+		const playlist = 'PLHtvRFLN5v-W-izd7V4JH2L4-RTW0WRi3';
+		const num = 8;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+		axios.get(url).then((json) => {
+			console.log(json.data.items);
+			setVids(json.data.items);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Youtube'}>
 			<p>Youtube Content</p>
