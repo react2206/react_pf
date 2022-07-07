ed375237bd5b0d929fba8d930429d7e55dee2f76
tubi55 <tubi55@nate.com>
2022-07-07 11:30:27 +0900

    axios로 호출한 데이터 state에 옮겨담기

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,6 +1,22 @@
+//npm i axios
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Department() {
+	const path = process.env.PUBLIC_URL;
+	const [Members, setMembers] = useState([]);
+
+	useEffect(() => {
+		axios.get(`${path}/DB/members.json`).then((json) => {
+			setMembers(json.data.members);
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log(Members);
+	}, [Members]);
+
 	return (
 		<Layout name={'Department'}>
 			<p>Department content</p>
