d7d57574b7b95175c8b27f57c362cf9db60e27e0
tubi55 <tubi55@nate.com>
2022-07-11 10:30:08 +0900

    마커 이미지 변경

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -9,18 +9,26 @@ function Location() {
 		level: 3,
 	};
 
-	//마커 위치 인스턴스 생성
 	const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+	//마커이미지 변경에 필요한 정보값 3개 등록
+	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+	const imageSize = new kakao.maps.Size(232, 99);
+	const imageOption = { offset: new kakao.maps.Point(116, 99) };
 
-	//위치 인스턴스 값을 인수로 전달해서 다시 마커 인스턴스 생성
+	//위의 정보값을 토대로 마커이미지 인스턴스 생성
+	const markerImage = new kakao.maps.MarkerImage(
+		imageSrc,
+		imageSize,
+		imageOption
+	);
+
 	const marker = new kakao.maps.Marker({
 		position: markerPosition,
+		image: markerImage,
 	});
 
 	useEffect(() => {
-		//지도 인스턴스 최종 생성
 		const map_instance = new kakao.maps.Map(container.current, option);
-		//지도 인스턴스를 활용해 마커 생성
 		marker.setMap(map_instance);
 	}, []);
 
