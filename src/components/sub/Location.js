b4264738cc193f01b8bfeae2d46456b90f941c49
tubi55 <tubi55@nate.com>
2022-07-11 10:16:34 +0900

    마커 인스턴스 생성

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -2,15 +2,26 @@ import Layout from '../common/Layout';
 import { useRef, useEffect } from 'react';
 
 function Location() {
-	const { kakao } = window; //윈도우 객체에 등록되어 있는 kakao키에 담겨있는 값을 변수명으로 비구조화할당
+	const { kakao } = window;
 	const container = useRef(null);
 	const option = {
 		center: new kakao.maps.LatLng(33.450701, 126.570667),
 		level: 3,
 	};
 
+	//마커 위치 인스턴스 생성
+	const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+	//위치 인스턴스 값을 인수로 전달해서 다시 마커 인스턴스 생성
+	const marker = new kakao.maps.Marker({
+		position: markerPosition,
+	});
+
 	useEffect(() => {
-		new kakao.maps.Map(container.current, option);
+		//지도 인스턴스 최종 생성
+		const map_instance = new kakao.maps.Map(container.current, option);
+		//지도 인스턴스를 활용해 마커 생성
+		marker.setMap(map_instance);
 	}, []);
 
 	return (
