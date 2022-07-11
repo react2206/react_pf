5b42959573df0aa2058a70697de58c1d726068fd
tubi55 <tubi55@nate.com>
2022-07-11 11:13:17 +0900

    버튼 하나로 Traffic 토글 처리

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -4,8 +4,9 @@ import { useRef, useEffect, useState } from 'react';
 function Location() {
 	const { kakao } = window;
 	const container = useRef(null);
-	//useEffect에서 만들어진 지도 인스턴스를  담을 state생성
 	const [Location, setLocation] = useState(null);
+	//Traffic 토글 기능 구현을 위한 state추가
+	const [Traffic, setTraffic] = useState(false);
 
 	const option = {
 		center: new kakao.maps.LatLng(33.450701, 126.570667),
@@ -31,25 +32,25 @@ function Location() {
 	useEffect(() => {
 		const map_instance = new kakao.maps.Map(container.current, option);
 		marker.setMap(map_instance);
-		//맵인스턴스를 컴포넌트에서 자유롭게 쓰기 위해서 state에 저장
 		setLocation(map_instance);
 	}, []);
+
+	//Traffic state값이 변경될때마다 실행될 구문
+	useEffect(() => {
+		//Location state값을 두번째 재호출 사이클부터 값이 담기므로 초기 오류방지를 위해 조건문 처리
+		if (!Location) return;
+		Traffic
+			? Location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: Location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
 
 	return (
 		<Layout name={'Location'}>
 			<div id='map' ref={container}></div>
-
-			<button
-				onClick={() => {
-					Location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-				}}>
-				Traffic ON
-			</button>
-			<button
-				onClick={() => {
-					Location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-				}}>
-				Traffic OFF
+			{/* 버튼 클릭시 기존의 Traffic값을 반전처리 */}
+			<button onClick={() => setTraffic(!Traffic)}>
+				{/* Traffic값에 따라 버튼의 내용도 변경 */}
+				{Traffic ? 'Traffic OFF' : 'Traffic ON'}
 			</button>
 		</Layout>
 	);
