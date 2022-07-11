397a5597674bcb476de4d2d428e9bda30c51bdb4
tubi55 <tubi55@nate.com>
2022-07-11 16:25:34 +0900

    기본 지도 출력

--- src/components/sub/Location2.js
+++ src/components/sub/Location2.js
@@ -1,7 +1,24 @@
 import Layout from '../common/Layout';
+import { useRef, useEffect } from 'react';
 
 function Location2() {
-	return <Layout name={'Location2'}>Location2</Layout>;
+	const { kakao } = window;
+	const container = useRef(null);
+	const mapOption = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level: 3, // 지도의 확대 레벨
+	};
+
+	useEffect(() => {
+		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+		const map_result = new kakao.maps.Map(container.current, mapOption);
+	}, []);
+
+	return (
+		<Layout name={'Location2'}>
+			<div id='map2' ref={container}></div>
+		</Layout>
+	);
 }
 
 export default Location2;
