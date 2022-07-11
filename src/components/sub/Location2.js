c5b067fb5c57928c98950c30c81c9b5ce3ca0888
tubi55 <tubi55@nate.com>
2022-07-11 16:34:56 +0900

    마커출력

--- src/components/sub/Location2.js
+++ src/components/sub/Location2.js
@@ -10,8 +10,18 @@ function Location2() {
 	};
 
 	useEffect(() => {
-		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 		const map_result = new kakao.maps.Map(container.current, mapOption);
+
+		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+		const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+		// 마커를 생성합니다
+		const marker = new kakao.maps.Marker({
+			position: markerPosition,
+		});
+
+		// 마커가 지도 위에 표시되도록 설정합니다
+		marker.setMap(map_result);
 	}, []);
 
 	return (
