d60b9123b2033962b20b7332238751bd074c5322
tubi55 <tubi55@nate.com>
2022-07-11 10:43:03 +0900

    버튼 클릭시 교통정보량 표시, 제거

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,21 +1,22 @@
 import Layout from '../common/Layout';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 function Location() {
 	const { kakao } = window;
 	const container = useRef(null);
+	//useEffect에서 만들어진 지도 인스턴스를  담을 state생성
+	const [Location, setLocation] = useState(null);
+
 	const option = {
 		center: new kakao.maps.LatLng(33.450701, 126.570667),
 		level: 3,
 	};
 
 	const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
-	//마커이미지 변경에 필요한 정보값 3개 등록
 	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
 	const imageSize = new kakao.maps.Size(232, 99);
 	const imageOption = { offset: new kakao.maps.Point(116, 99) };
 
-	//위의 정보값을 토대로 마커이미지 인스턴스 생성
 	const markerImage = new kakao.maps.MarkerImage(
 		imageSrc,
 		imageSize,
@@ -30,11 +31,26 @@ function Location() {
 	useEffect(() => {
 		const map_instance = new kakao.maps.Map(container.current, option);
 		marker.setMap(map_instance);
+		//맵인스턴스를 컴포넌트에서 자유롭게 쓰기 위해서 state에 저장
+		setLocation(map_instance);
 	}, []);
 
 	return (
 		<Layout name={'Location'}>
 			<div id='map' ref={container}></div>
+
+			<button
+				onClick={() => {
+					Location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+				}}>
+				Traffic ON
+			</button>
+			<button
+				onClick={() => {
+					Location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+				}}>
+				Traffic OFF
+			</button>
 		</Layout>
 	);
 }
