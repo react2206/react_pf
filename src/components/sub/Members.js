7b91c4793728dd192bc9f2f7bcfa54caf7d845f5
tubi55 <tubi55@nate.com>
2022-07-12 10:47:38 +0900

    기본 폼 인증 흐름 순서 주석버전

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -6,14 +6,11 @@ function Members() {
 		userid: '',
 	};
 	const [Val, setVal] = useState(initVal);
-	//인증조건실패시 출력될 에러 메세지가 항목별로 담길 state추가
 	const [Err, setErr] = useState({});
 
-	//인증처리 함수
+	//순서7- 인수로 받은 state값을 검사해서 조건이 불일치하면 내부 지역변수 객체에 에러문구를 담아서 리턴
 	const check = (value) => {
 		const errs = {};
-
-		//userid 인증처리
 		if (value.userid.length < 5) {
 			errs.userid = '아이디를 5글자 이상 입력하세요.';
 		}
@@ -21,25 +18,29 @@ function Members() {
 	};
 
 	const handleChange = (e) => {
+		//순서2- 입력하고 있는 input요소의 name, value값을 변수로 빕구조화 할당
 		const { name, value } = e.target;
+		//순서3 - 비구조화할당으로 받은 값을 Val 스테이트에 저장
+		//이때 객체 키값을 변수로 활용하기 위해서 []감싸줌
 		setVal({ ...Val, [name]: value });
 	};
 
-	//submit이벤트 발생시
 	const handleSubmit = (e) => {
-		//일단은 서버전송을 막아주고
+		//순서6 - 일단 서버전송의 기본 이벤트 기능 막아주고
+		//check함수에 Val스테이트값을 인수로 전달해 인증검사 시작
 		e.preventDefault();
-		//check함수에 인수로 Val값을 넣어서 인증검사후
-		//반환된 에러객체값을 Err state에 옮겨담음
+		//순서8- 반환된 에러객체를 Err스테이트에 옮겨담음
 		setErr(check(Val));
 	};
 
+	//순서9 Err객체값이 변경될때마다 콘솔출력
 	useEffect(() => {
 		console.log(Err);
 	}, [Err]);
 
 	return (
 		<Layout name={'Members'}>
+			{/* 순서5 - 전송버튼 클릭시 handleSubmit함수 호출 */}
 			<form onSubmit={handleSubmit}>
 				<fieldset>
 					<legend>회원가입 폼 양식</legend>
@@ -57,10 +58,12 @@ function Members() {
 										placeholder='아이디를 입력하세요'
 										name='userid'
 										id='userid'
+										//순서4 - Val 스테이트값이 변경될때마다 input화면에 출력
 										value={Val.userid}
+										//순서1 - 인풋에 값 입력시 handleChange함수 호출
 										onChange={handleChange}
 									/>
-									{/* 혹시 에러가 있으면 Err정보값 화면에 출력 */}
+									{/* 순서10 - 만약 에러객체가 생기면 화면에 출력 */}
 									<span className='err'>{Err.userid}</span>
 								</td>
 							</tr>
