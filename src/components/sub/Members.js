832cd10e6858b10e517c293a0f1cc754fee5421b
tubi55 <tubi55@nate.com>
2022-07-12 11:38:07 +0900

    비밀번호 인증처리

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 function Members() {
 	const initVal = {
 		userid: '',
+		pwd1: '',
+		pwd2: '',
 		email: '',
 	};
 	const [Val, setVal] = useState(initVal);
@@ -11,6 +13,11 @@ function Members() {
 
 	const check = (value) => {
 		const errs = {};
+		//인증처리할 조건 정규표현식
+		const eng = /[a-zA-Z]/;
+		const num = /[0-9]/;
+		const spc = /[~!@#$%^&*()_+]/;
+
 		//userid인증처리
 		if (value.userid.length < 5) {
 			errs.userid = '아이디를 5글자 이상 입력하세요.';
@@ -18,6 +25,18 @@ function Members() {
 		//email인증처리
 		if (value.email.length < 8 || !/@/.test(Val.email)) {
 			errs.email = '이메일은 8글자이상 @를 포함하세요';
+		}
+		//pwd인증처리
+		if (
+			value.pwd1 < 5 ||
+			!eng.test(value.pwd1) ||
+			!num.test(value.pwd1) ||
+			!spc.test(value.pwd1)
+		) {
+			errs.pwd1 = '비밀번호는 5글자 이상, 영문,숫자,특수문자를 모두 포함하세요';
+		}
+		if (value.pwd1 !== value.pwd2 || value.pwd2 < 5) {
+			errs.pwd2 = '두개의 비밀번호를 동일하게 입력하세요';
 		}
 		return errs;
 	};
@@ -59,6 +78,38 @@ function Members() {
 										onChange={handleChange}
 									/>
 									<span className='err'>{Err.userid}</span>
+								</td>
+							</tr>
+
+							{/* password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd1'>PASSWORD</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										name='pwd1'
+										id='pwd1'
+										placeholder='비밀번호를 입력하세요'
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.pwd1}</span>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd2'>RE-PASSWORD</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										name='pwd2'
+										id='pwd2'
+										placeholder='비밀번호를 재입력하세요'
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.pwd2}</span>
 								</td>
 							</tr>
 
