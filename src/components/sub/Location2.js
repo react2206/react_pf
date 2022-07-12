f3268700a079d2a86d4f5375d6473cc9c554303d
tubi55 <tubi55@nate.com>
2022-07-12 09:48:08 +0900

    현재 입력하고 있는 input값을 state에 저장했다가 input의 value속성으로 출력

--- src/components/sub/Location2.js
+++ src/components/sub/Location2.js
@@ -3,37 +3,32 @@ import { useRef, useEffect } from 'react';
 
 function Location2() {
 	const { kakao } = window;
+	//지도가 들어갈 프레임 참조
 	const container = useRef(null);
+
+	//맵옵션 (맵위치, 맵확대축소 레벨)
 	const mapOption = {
-		center: new kakao.maps.LatLng(37.56704043757791, 126.97862955893478), // 지도의 중심좌표
-		level: 3, // 지도의 확대 레벨
+		center: new kakao.maps.LatLng(37.56704043757791, 126.97862955893478),
+		level: 3,
 	};
 
 	useEffect(() => {
+		//컴포넌트 마운트시 지도 가상돔 요소에 출력 (인수로 지도가 들어갈 프레임, 지도옵션)
 		const map_result = new kakao.maps.Map(container.current, mapOption);
 
-		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+		//마커 설정 및 추가
 		const markerPosition = new kakao.maps.LatLng(
 			37.56704043757791,
 			126.97862955893478
 		);
-
-		// 마커를 생성합니다
 		const marker = new kakao.maps.Marker({
 			position: markerPosition,
 		});
-
-		// 마커가 지도 위에 표시되도록 설정합니다
 		marker.setMap(map_result);
 
-		// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+		//맵에 컨트롤 패널 추가
 		const mapTypeControl = new kakao.maps.MapTypeControl();
-
-		// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-		// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
 		map_result.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPLEFT);
-
-		// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
 		const zoomControl = new kakao.maps.ZoomControl();
 		map_result.addControl(zoomControl, kakao.maps.ControlPosition.BOTTOMLEFT);
 	}, []);
--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -1,9 +1,46 @@
 import Layout from '../common/Layout';
+import { useState } from 'react';
 
 function Members() {
+	//userid의 입력값이 담길 초기 state를 객체로 지정
+	const initVal = {
+		userid: '',
+	};
+	//해당 객체값을 Val state에 초기값으로  저장
+	const [Val, setVal] = useState(initVal);
+
 	return (
 		<Layout name={'Members'}>
-			<p>Member contents</p>
+			<form>
+				<fieldset>
+					<legend>회원가입 폼 양식</legend>
+					<table border='1'>
+						<caption>회원가입 정보입력</caption>
+						<tbody>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>USER ID</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										placeholder='아이디를 입력하세요'
+										name='userid'
+										id='userid'
+										//Val state에 있는 userid값을 input요소에 출력
+										value={Val.userid}
+										onChange={(e) => {
+											console.log(e.target.value);
+											//onChange이벤트가 발생할때마다 기존Val state값을 복사해서 현재 입력하고 있는 값으로 바꿔치기
+											setVal({ ...Val, userid: e.target.value });
+										}}
+									/>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
