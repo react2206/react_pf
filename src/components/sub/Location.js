c810ca0baa98937c581ace0e9519129930b0abd6
tubi55 <tubi55@nate.com>
2022-07-12 09:06:34 +0900

    지도 컨트롤 추가(어려운버전)

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -89,6 +89,17 @@ function Location() {
 			map_instance.setCenter(Info[Index].latlng);
 		};
 
+		// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+		const mapTypeControl = new kakao.maps.MapTypeControl();
+
+		// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+		// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+		map_instance.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPLEFT);
+
+		// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+		const zoomControl = new kakao.maps.ZoomControl();
+		map_instance.addControl(zoomControl, kakao.maps.ControlPosition.BOTTOMLEFT);
+
 		//브라우저 리사이즈시 마커가운데 유지함수 호출
 		window.addEventListener('resize', handleResize);
 
