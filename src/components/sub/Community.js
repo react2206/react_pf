73bb26e9d123e332a4398b38783b8bb281180101
tubi55 <tubi55@nate.com>
2022-07-13 09:13:36 +0900

    write버튼 클릭시 input, textarea의 글을 저장할수 있는 핸들러함수 연결

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,9 +1,34 @@
 import Layout from '../common/Layout';
+import { useRef, useState, useEffect } from 'react';
 
 function Community() {
+	const input = useRef(null);
+	const textarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+
+	//글저장 함수
+	const createPost = () => {};
+
+	//Posts의 값이 변경될때마다 콘솔출력
+	useEffect(() => {
+		console.log(Posts);
+	}, [Posts]);
+
 	return (
 		<Layout name={'Community'}>
-			<p>Community Content</p>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요' ref={input} />
+				<br />
+				<textarea
+					cols='30'
+					rows='5'
+					placeholder='본문을 입력하세요'
+					ref={textarea}></textarea>
+				<br />
+
+				<button>CANCEL</button>
+				<button onClick={createPost}>WRITE</button>
+			</div>
 		</Layout>
 	);
 }
