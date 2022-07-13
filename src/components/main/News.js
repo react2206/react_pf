6e54452df7589b895f279041fd42e0f4aab92e7b
tubi55 <tubi55@nate.com>
2022-07-13 15:10:38 +0900

    메인페이지에 게시글 미리보기 출력

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
+
 function News() {
+	const [Posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		let data = localStorage.getItem('post');
+		data = JSON.parse(data);
+		setPosts(data);
+	}, []);
+
 	return (
 		<section id='news'>
 			<h1>News</h1>
+			{Posts.map((post, idx) => {
+				if (idx >= 5) return;
+
+				return (
+					<article key={idx}>
+						<h2>{post.title}</h2>
+						<p>{post.content}</p>
+					</article>
+				);
+			})}
 		</section>
 	);
 }
