4b1d37a2b8ffdc0e33d623af683fbe0dca08b774
tubi55 <tubi55@nate.com>
2022-07-14 11:05:56 +0900

    버튼 활성화지점 커스터마이징

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -12,7 +12,6 @@ function Main() {
 	const pos = useRef([]);
 	const [Index, setIndex] = useState(0);
 
-	//섹션의 세로 위치값을 구하는 함수
 	const getPos = () => {
 		pos.current = [];
 		const secs = main.current.querySelectorAll('.myScroll');
@@ -20,18 +19,14 @@ function Main() {
 		console.log(pos.current);
 	};
 
-	//스크롤 위치에 따라서 버튼 활성화 함수
 	const activation = () => {
+		const base = -window.innerHeight / 2;
 		const scroll = window.scrollY;
 		const btns = main.current.querySelectorAll('.scroll_navi li');
 
-		//pos.current배열에 등록된 각 섹션의 세로 위치값을 반복을 돌면서
 		pos.current.map((pos, idx) => {
-			//현재 스크롤된 거리값의 각 섹션의 위치값보다 같거나 크면
-			if (scroll >= pos) {
-				//일단 모든 버튼의 on을 지워서 비활성화
+			if (scroll >= pos + base) {
 				for (const btn of btns) btn.classList.remove('on');
-				//현재 idx순번의 버튼만 on을 붙여서 활성화
 				btns[idx].classList.add('on');
 			}
 		});
@@ -47,8 +42,6 @@ function Main() {
 		};
 	}, []);
 
-	//자식 Btns컴포넌트를 통해서 Index 값이 변경될때마다
-	//pos.current 배열값에 저장한 세로위치값으로 new Anime 스크롤 모션처리
 	useEffect(() => {
 		new Anime(window, {
 			prop: 'scroll',
