6d80583c51a221240c9e10ed0546773e41c87457
tubi55 <tubi55@nate.com>
2022-07-14 15:12:58 +0900

    swiper에 pagination버튼 추가

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,10 +1,20 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
 import 'swiper/css';
+import 'swiper/css/pagination';
 
 function Vids() {
 	return (
 		<section id='vids' className='myScroll'>
-			<Swiper>
+			<Swiper
+				modules={[Pagination]}
+				pagination={{
+					clickable: true,
+				}}
+				spaceBetween={60}
+				loop={true}
+				slidesPerView={3}
+				centeredSlides={true}>
 				<SwiperSlide>Slide 1</SwiperSlide>
 				<SwiperSlide>Slide 2</SwiperSlide>
 				<SwiperSlide>Slide 3</SwiperSlide>
