f8fc314cafe91a3c39c754bcb84e1f8c3a19716e
tubi55 <tubi55@nate.com>
2022-07-14 09:20:10 +0900

    리액트에서 Anim.js 기본 사용법

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import Anime from '../../asset/anim';
+import { useRef } from 'react';
+
+const style = {
+	width: 100,
+	height: 100,
+	backgroundColor: 'aqua',
+	position: 'absolute',
+	top: 200,
+	left: 100,
+};
+
+const btn = {
+	position: 'absolute',
+	top: 120,
+	left: 100,
+};
 
 function Visual() {
-	return <figure id='visual'></figure>;
+	const box = useRef(null);
+
+	return (
+		<figure id='visual'>
+			<button
+				style={btn}
+				onClick={() => {
+					new Anime(box.current, {
+						prop: 'left',
+						value: 1000,
+						duration: 500,
+						callback: () => {
+							new Anime(box.current, {
+								prop: 'top',
+								value: 500,
+								duration: 500,
+							});
+						},
+					});
+				}}>
+				button
+			</button>
+			<div className='box' style={style} ref={box}></div>
+		</figure>
+	);
 }
 
 export default Visual;
