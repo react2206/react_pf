3c62695431960d0a7cff03fde9a94d69c44a27ab
tubi55 <tubi55@nate.com>
2022-07-14 14:50:01 +0900

    기본 swiper적용

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,7 +1,16 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 function Vids() {
 	return (
 		<section id='vids' className='myScroll'>
-			<h1>Vids</h1>
+			<Swiper>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+			</Swiper>
 		</section>
 	);
 }
