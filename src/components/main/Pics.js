821535c75ac88385f565fba4591777b84d3fc4c9
tubi55 <tubi55@nate.com>
2022-07-14 14:19:54 +0900

    스크롤 스타일 연동시 시간차 적용

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -4,15 +4,17 @@ function Pics({ Scrolled, start, base }) {
 	return (
 		<section id='pics' className='myScroll'>
 			<p
-				style={
-					position >= 0
-						? {
-								left: 100 + position,
-						  }
-						: null
-				}>
+				style={{
+					left: 100 + position,
+				}}>
 				FLICKR
 			</p>
+			<h3
+				style={{
+					left: 100 + position / 2,
+				}}>
+				FLICKR
+			</h3>
 		</section>
 	);
 }
