c1eea3946723001f1faa6724a746f238205f9eed
tubi55 <tubi55@nate.com>
2022-07-14 15:38:35 +0900

    버튼 스타일링

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,16 +1,18 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Pagination, Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 function Vids() {
 	return (
 		<section id='vids' className='myScroll'>
 			<Swiper
-				modules={[Pagination]}
+				modules={[Pagination, Navigation]}
 				pagination={{
 					clickable: true,
 				}}
+				navigation={true}
 				spaceBetween={60}
 				loop={true}
 				slidesPerView={3}
