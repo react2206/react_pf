690c0b2154a50b6d74ac5a9e844d55b0602ea4b6
tubi55 <tubi55@nate.com>
2022-07-14 09:33:25 +0900

    Anime로 스크롤 제어

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,45 +1,25 @@
 import Anime from '../../asset/anim';
 import { useRef } from 'react';
 
-const style = {
-	width: 100,
-	height: 100,
-	backgroundColor: 'aqua',
-	position: 'absolute',
-	top: 200,
-	left: 100,
-};
-
-const btn = {
-	position: 'absolute',
-	top: 120,
-	left: 100,
-};
-
 function Visual() {
-	const box = useRef(null);
-
+	const btnStyle = {
+		position: 'absolute',
+		top: 150,
+		left: 100,
+	};
 	return (
 		<figure id='visual'>
 			<button
-				style={btn}
+				style={btnStyle}
 				onClick={() => {
-					new Anime(box.current, {
-						prop: 'left',
+					new Anime(window, {
+						prop: 'scroll',
 						value: 1000,
 						duration: 500,
-						callback: () => {
-							new Anime(box.current, {
-								prop: 'top',
-								value: 500,
-								duration: 500,
-							});
-						},
 					});
 				}}>
-				button
+				scroll
 			</button>
-			<div className='box' style={style} ref={box}></div>
 		</figure>
 	);
 }
