334b96ef7b91110cc32102c8838d1bdfbea84587
tubi55 <tubi55@nate.com>
2022-07-15 10:18:57 +0900

    갤러리 전환

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -6,28 +6,44 @@ function Gallery() {
 	const frame = useRef(null);
 	const [Items, setItems] = useState([]);
 
-	//wrapping함수에 async 키워드를 붙이고
-	//그 안쪽에서 비동기로 실행되는 구문 앞에 await를 붙이면 동기화됨
-	const getFlickr = async () => {
-		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
-		const method_interest = 'flickr.interestingness.getList';
-		const num = 20;
-		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
+	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+	const method_interest = 'flickr.interestingness.getList';
+	const method_search = 'flickr.photos.search';
+	const num = 20;
+	const interest_url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
+	const search_url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&tags=${'ocean'}`;
 
+	//url파라미터에 따라 결과값 달라지도록 수정
+	const getFlickr = async (url) => {
 		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
-
 		frame.current.classList.add('on');
 	};
 
 	useEffect(() => {
-		getFlickr();
+		getFlickr(interest_url);
 	}, []);
 
 	return (
 		<Layout name={'Gallery'}>
+			<button
+				onClick={() => {
+					frame.current.classList.remove('on');
+					getFlickr(interest_url);
+				}}>
+				Interest Gallery
+			</button>
+
+			<button
+				onClick={() => {
+					frame.current.classList.remove('on');
+					getFlickr(search_url);
+				}}>
+				Search Gallery
+			</button>
+
 			<div className='frame' ref={frame}>
 				{Items.map((item, idx) => {
 					return (
