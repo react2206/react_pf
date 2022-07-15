62894b0e768e490f849d925fbddabba07f5baf5f
tubi55 <tubi55@nate.com>
2022-07-15 09:54:35 +0900

    async await으로 동기화 처리

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -4,22 +4,31 @@ import { useEffect, useState, useRef } from 'react';
 
 function Gallery() {
 	const frame = useRef(null);
-	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 20;
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
 	const [Items, setItems] = useState([]);
 
-	useEffect(() => {
-		axios.get(url).then((json) => {
+	//wrapping함수에 async 키워드를 붙이고
+	//그 안쪽에서 비동기로 실행되는 구문 앞에 await를 붙이면 동기화됨
+	const getFlickr = async () => {
+		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 20;
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
+
+		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
+
+		frame.current.classList.add('on');
+	};
+
+	useEffect(() => {
+		getFlickr();
 	}, []);
 
 	return (
 		<Layout name={'Gallery'}>
-			<div className='frame on' ref={frame}>
+			<div className='frame' ref={frame}>
 				{Items.map((item, idx) => {
 					return (
 						<article key={idx}>
