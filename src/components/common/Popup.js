2371902cecf303e3dbc9af241c629b01ab86f04c
tubi55 <tubi55@nate.com>
2022-07-18 09:37:51 +0900

    framer-motion으로 팝업 애니메이션

--- src/components/common/Popup.js
+++ src/components/common/Popup.js
@@ -1,4 +1,5 @@
 import { useEffect, useState, forwardRef, useImperativeHandle } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Popup = forwardRef((props, ref) => {
 	const [Open, setOpen] = useState(false);
@@ -16,16 +17,39 @@ const Popup = forwardRef((props, ref) => {
 	});
 
 	return (
-		<>
+		<AnimatePresence>
 			{Open && (
-				<aside className='pop'>
-					<div className='con'>{props.children}</div>
-					<span className='close' onClick={() => setOpen(false)}>
+				<motion.aside
+					className='pop'
+					initial={{ opacity: 0, scale: 0 }}
+					animate={{ opacity: 1, scale: 1, transition: { duration: 0.5 } }}
+					exit={{
+						opacity: 0,
+						scale: 0,
+						transition: { duration: 0.5, delay: 0.5 },
+					}}>
+					<motion.div
+						className='con'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1, transition: { duration: 0.5, delay: 0.5 } }}
+						exit={{ opacity: 0, transition: { duration: 0.5 } }}>
+						{props.children}
+					</motion.div>
+					<motion.span
+						className='close'
+						onClick={() => setOpen(false)}
+						initial={{ x: 100, opacity: 0 }}
+						animate={{
+							x: 0,
+							opacity: 1,
+							transition: { duration: 0.5, delay: 0.5 },
+						}}
+						exit={{ opacity: 0, x: 100 }}>
 						close
-					</span>
-				</aside>
+					</motion.span>
+				</motion.aside>
 			)}
-		</>
+		</AnimatePresence>
 	);
 });
 
