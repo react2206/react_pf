b10f701d117b5ce5317996a4a3db34f27e55e54a
tubi55 <tubi55@nate.com>
2022-07-18 15:39:42 +0900

    메인페이지 영상추가, 서브페이지 비주얼 이미지 삽입

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -9,7 +9,12 @@ function Layout(props) {
 
 	return (
 		<section className={`content ${props.name}`} ref={frame}>
-			<figure></figure>
+			<figure>
+				<img
+					src={`${process.env.PUBLIC_URL}/img/${props.name}.jpg`}
+					alt={props.name}
+				/>
+			</figure>
 			<div className='inner'>
 				<h1>{props.name}</h1>
 				{props.children}
--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -2,7 +2,15 @@ import Anime from '../../asset/anim';
 import { useRef } from 'react';
 
 function Visual() {
-	return <figure id='visual' className='myScroll on'></figure>;
+	return (
+		<figure id='visual' className='myScroll on'>
+			<video
+				loop
+				muted
+				autoPlay
+				src={process.env.PUBLIC_URL + '/img/vid.mp4'}></video>
+		</figure>
+	);
 }
 
 export default Visual;
