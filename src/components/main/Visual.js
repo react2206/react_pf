3d7caf38c698fcf842c58a1ec252e071f2430f44
tubi55 <tubi55@nate.com>
2022-07-19 16:08:57 +0900

    패널 스타일링

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -4,11 +4,24 @@ import { useRef } from 'react';
 function Visual() {
 	return (
 		<figure id='visual' className='myScroll on'>
-			<video
-				loop
-				muted
-				autoPlay
-				src={process.env.PUBLIC_URL + '/img/vid.mp4'}></video>
+			<ul className='panel'>
+				<li className='s1 on'>1</li>
+				<li className='s2'>2</li>
+				<li className='s3'>3</li>
+				<li className='s4'>4</li>
+				<li className='s5'>5</li>
+			</ul>
+
+			<ul className='navi'>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+
+			<button className='prev'>prev</button>
+			<button className='next'>next</button>
 		</figure>
 	);
 }
