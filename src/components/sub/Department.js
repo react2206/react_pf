bab239aa5ff1b0991afda99a68e54dba5d0663a4
tubi55 <tubi55@nate.com>
2022-07-20 10:49:55 +0900

    store의 데이터 전역에서 useSelector로 데이터 파싱

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,31 +1,21 @@
 //npm i axios
 import Layout from '../common/Layout';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function Department() {
 	const path = process.env.PUBLIC_URL;
-	const [Members, setMembers] = useState([]);
-
-	useEffect(() => {
-		axios.get(`${path}/DB/members.json`).then((json) => {
-			setMembers(json.data.members);
-		});
-	}, []);
+	const members = useSelector((store) => store.memberReducer.members);
 
 	return (
 		<Layout name={'Department'}>
-			{Members.map((member, idx) => {
+			{members.map((member, idx) => {
 				return (
 					<article key={idx}>
 						<div className='inner'>
 							<div className='picFrame'>
-								{/* blured shadow */}
 								<div className='shadow'>
 									<img src={`${path}/img/${member.pic}`} alt={member.name} />
 								</div>
-
-								{/* pic  */}
 								<div className='pic'>
 									<img src={`${path}/img/${member.pic}`} alt={member.name} />
 								</div>
