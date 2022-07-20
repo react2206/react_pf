5a728134241b8f09ea676a769cc961261313bd81
tubi55 <tubi55@nate.com>
2022-07-20 10:34:56 +0900

    member전용 액션생성함수 추가 및 해당 리듀서 추가

---
+++ src/redux/reducers.js
@@ -0,0 +1,52 @@
+import { combineReducers } from 'redux';
+
+const initMember = {
+	members: [
+		{
+			name: 'Julia',
+			position: 'President',
+			pic: 'member1.jpg',
+		},
+		{
+			name: 'David',
+			position: 'Vice President',
+			pic: 'member2.jpg',
+		},
+		{
+			name: 'Emily',
+			position: 'UI Designer',
+			pic: 'member3.jpg',
+		},
+		{
+			name: 'Paul',
+			position: 'Front-end Engineer',
+			pic: 'member4.jpg',
+		},
+		{
+			name: 'Sara',
+			position: 'Back-end Engineer',
+			pic: 'member5.jpg',
+		},
+		{
+			name: 'Michael',
+			position: 'Project Manager',
+			pic: 'member6.jpg',
+		},
+	],
+};
+
+const memberReducer = (state = initMember, action) => {
+	switch (action.type) {
+		case 'SET_MEMBERS':
+			return { ...state, members: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+const reducers = combineReducers({
+	memberReducer,
+});
+
+export default reducers;
