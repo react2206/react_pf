a5e7cd5a4c980547d9922f1d9ef197ea2c76d76a
tubi55 <tubi55@nate.com>
2022-07-21 10:26:20 +0900

    flickr 데이터 호출 구문을 api.js에 순수함수 형태로 분리

---
+++ src/redux/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const getFlickr = async (opt) => {
+	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+	const method_interest = 'flickr.interestingness.getList';
+	const method_search = 'flickr.photos.search';
+	const method_user = 'flickr.people.getPhotos';
+	const num = 500;
+	let url = '';
+
+	if (opt.type === 'interest') {
+		url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
+	}
+	if (opt.type === 'search') {
+		url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&tags=${opt.tags}`;
+	}
+	if (opt.type === 'user') {
+		url = `https://www.flickr.com/services/rest/?method=${method_user}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&user_id=${opt.user}`;
+	}
+
+	await axios.get(url);
+};
