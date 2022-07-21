0c040f39809635359c5829e5d8bb2c49b840a441
tubi55 <tubi55@nate.com>
2022-07-21 12:47:05 +0900

    데이터 새로 호출시마다 startLoading으로 로딩바 보임처리, flickr 데이터 변경완료시 endLoading 으로 로딩바 숨김처리

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -9,12 +9,30 @@ function Flickr() {
 	const input = useRef(null);
 	const { flickr } = useSelector((store) => store.flickrReducer);
 	const [Opt, setOpt] = useState({ type: 'user', user: '164021883@N04' });
+	const [Loading, setLoading] = useState(true);
 	const masonryOptions = { transitionDuration: '0.5s' };
+
+	//로딩바 출력하면서 기존 프레임 숨기는 함수
+	const startLoading = () => {
+		setLoading(true);
+		frame.current.classList.remove('on');
+	};
+
+	//로딩바 제거하고 다시 프레임 보이는 함수
+	const endLoading = () => {
+		setTimeout(() => {
+			frame.current.classList.add('on');
+			setLoading(false);
+		}, 1000);
+	};
 
 	const showSearch = () => {
 		const result = input.current.value.trim();
 		input.current.value = '';
 		if (!result) return alert('검색어를 입력하세요.');
+		//showSearch함수가 호출될떄 setOpt데이터 요청하기전
+		//startLoaing함수로 로딩화면 처리
+		startLoading();
 
 		setOpt({
 			type: 'search',
@@ -26,11 +44,28 @@ function Flickr() {
 		dispatch({ type: 'FLICKR_START', Opt });
 	}, [Opt]);
 
+	//flickr state가 변경될때마다 endLoading함수 호출해서 로딩바 숨기고 화면출력
+	//flickr값이 변경된다는 의미는 store로 부터 변경요청한 비동기데이터가 로딩완료됐다는 의미
+	useEffect(endLoading, [flickr]);
+
 	return (
 		<Layout name={'Flickr'}>
-			<button onClick={() => setOpt({ type: 'interest' })}>
+			{Loading && (
+				<img
+					className='loading'
+					src={`${process.env.PUBLIC_URL}/img/loading.gif`}
+				/>
+			)}
+
+			<button
+				onClick={() => {
+					//interest갤러리 버튼 클릭시 로딩바 보이기
+					startLoading();
+					setOpt({ type: 'interest' });
+				}}>
 				Interest Gallery
 			</button>
+
 			<div className='searchBox'>
 				<input
 					type='text'
@@ -69,6 +104,8 @@ function Flickr() {
 										/>
 										<span
 											onClick={(e) => {
+												//사용자 아이디 클릭시 로딩바 보이기
+												startLoading();
 												setOpt({ type: 'user', user: e.target.innerText });
 											}}>
 											{pic.owner}
