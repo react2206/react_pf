b6210fd532e7352009a85ee44607e52f141688e3
tubi55 <tubi55@nate.com>
2022-07-21 14:40:00 +0900

    팝업 호출

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -1,4 +1,5 @@
 import Layout from '../common/Layout';
+import Popup from '../common/Popup';
 import { useRef, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Masonry from 'react-masonry-component';
@@ -6,9 +7,11 @@ import Masonry from 'react-masonry-component';
 function Flickr() {
 	const dispatch = useDispatch();
 	const frame = useRef(null);
+	const pop = useRef(null);
 	const input = useRef(null);
 	const { flickr } = useSelector((store) => store.flickrReducer);
 	const [Opt, setOpt] = useState({ type: 'user', user: '164021883@N04' });
+	const [Index, setIndex] = useState(0);
 	const [Loading, setLoading] = useState(true);
 	const [EnableClick, setEnableClick] = useState(true);
 	const masonryOptions = { transitionDuration: '0.5s' };
@@ -22,18 +25,13 @@ function Flickr() {
 		setTimeout(() => {
 			frame.current.classList.add('on');
 			setLoading(false);
-			//frame에 on이 붙어서 1초동안 화면 출력 모션이 끝나면 EnableClick값을 true로 변경해서 다시 이벤트 발생가능하게 변경
 			setTimeout(() => setEnableClick(true), 1000);
 		}, 1000);
 	};
 
-	//각 핸들러 함수 호출될때
 	const showInterest = () => {
-		//EnableClick이 true가 아니면 함수 종료
 		if (!EnableClick) return;
-		//true면 false로 변경해서 재이벤트 발생 못하게 막아놓고 아래코드 실행
 		setEnableClick(false);
-
 		startLoading();
 		setOpt({ type: 'interest' });
 	};
@@ -41,7 +39,6 @@ function Flickr() {
 	const showSearch = () => {
 		if (!EnableClick) return;
 		setEnableClick(false);
-
 		const result = input.current.value.trim();
 		input.current.value = '';
 		if (!result) return alert('검색어를 입력하세요.');
@@ -63,6 +60,11 @@ function Flickr() {
 		setOpt({ type: 'user', user: userID });
 	};
 
+	const openPop = (index) => {
+		pop.current.open();
+		setIndex(index);
+	};
+
 	useEffect(() => {
 		dispatch({ type: 'FLICKR_START', Opt });
 	}, [Opt]);
@@ -70,62 +72,73 @@ function Flickr() {
 	useEffect(endLoading, [flickr]);
 
 	return (
-		<Layout name={'Flickr'}>
-			{Loading && (
-				<img
-					className='loading'
-					src={`${process.env.PUBLIC_URL}/img/loading.gif`}
-				/>
-			)}
+		<>
+			<Layout name={'Flickr'}>
+				{Loading && (
+					<img
+						className='loading'
+						src={`${process.env.PUBLIC_URL}/img/loading.gif`}
+					/>
+				)}
 
-			<button onClick={showInterest}>Interest Gallery</button>
-			<button onClick={showUser}>My Gallery</button>
+				<button onClick={showInterest}>Interest Gallery</button>
+				<button onClick={showUser}>My Gallery</button>
 
-			<div className='searchBox'>
-				<input
-					type='text'
-					ref={input}
-					placeholder='검색어를 입력하세요.'
-					onKeyUp={(e) => {
-						if (e.key === 'Enter') showSearch();
-					}}
-				/>
-				<button onClick={showSearch}>SEARCH</button>
-			</div>
+				<div className='searchBox'>
+					<input
+						type='text'
+						ref={input}
+						placeholder='검색어를 입력하세요.'
+						onKeyUp={(e) => {
+							if (e.key === 'Enter') showSearch();
+						}}
+					/>
+					<button onClick={showSearch}>SEARCH</button>
+				</div>
 
-			<div className='frame' ref={frame}>
-				<Masonry elementType={'div'} options={masonryOptions}>
-					{flickr.map((pic, idx) => {
-						return (
-							<article key={idx}>
-								<div className='inner'>
-									<div className='pic'>
-										<img
-											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-											alt={pic.title}
-										/>
+				<div className='frame' ref={frame}>
+					<Masonry elementType={'div'} options={masonryOptions}>
+						{flickr.map((pic, idx) => {
+							return (
+								<article key={idx}>
+									<div className='inner'>
+										<div className='pic' onClick={() => openPop(idx)}>
+											<img
+												src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+												alt={pic.title}
+											/>
+										</div>
+										<h2>{pic.title}</h2>
+										<div className='profile'>
+											<img
+												src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+												alt={pic.owner}
+												onError={(e) => {
+													e.target.setAttribute(
+														'src',
+														'https://www.flickr.com/images/buddyicon.gif'
+													);
+												}}
+											/>
+											<span onClick={showUser}>{pic.owner}</span>
+										</div>
 									</div>
-									<h2>{pic.title}</h2>
-									<div className='profile'>
-										<img
-											src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-											alt={pic.owner}
-											onError={(e) => {
-												e.target.setAttribute(
-													'src',
-													'https://www.flickr.com/images/buddyicon.gif'
-												);
-											}}
-										/>
-										<span onClick={showUser}>{pic.owner}</span>
-									</div>
-								</div>
-							</article>
-						);
-					})}
-				</Masonry>
-			</div>
-		</Layout>
+								</article>
+							);
+						})}
+					</Masonry>
+				</div>
+			</Layout>
+
+			<Popup ref={pop}>
+				{flickr.length !== 0 && (
+					<img
+						src={`https://live.staticflickr.com/${flickr[Index].server}/${flickr[Index].id}_${flickr[Index].secret}_b.jpg`}
+						alt={flickr[Index].title}
+					/>
+				)}
+			</Popup>
+		</>
 	);
 }
 
